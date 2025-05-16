ed2169d0de0703d32a0f53239793c3033be13d83
scott carver <scott@artificia.org>
2025-05-16 21:25:44 +0200

    Improve bot command impl and add poems command

---
+++ poems.py
@@ -0,0 +1,771 @@
+poems = [
+    """
+At Night The States 
+by Alice Notley
+
+
+I forget them or I wish I was there
+in that one under the
+Stars. It smells like June in this night
+so sweet like air.
+I may have decided that the
+States are not that tired
+Or I have thought so. I have
+thought that.
+At night the states
+And the world not that tired
+of everyone
+Maybe. Honey, I think that to
+say is in
+light. Or whoever. We will
+never
+replace You. We will never re-
+place You. But
+in like a dream the floor is no
+longer discursive
+To me it doesn't please me by
+being the vistas out my
+window, do you know what
+Of course (not) I mean?
+I have no dreams of wake-
+fulness. In
+wakefulness. And so to begin.
+(my love.)
+At night the states
+talk. My initial continuing contra-
+diction
+my love for you & that for me
+deep down in the Purple Plant the oldest
+dust
+of it is sweetest but sates no longer
+how I
+would feel. Shirt
+that shirt has been in your arms
+And I have
+that shirt is how I feel
+At night the states
+will you continue in this as-
+sociation of
+matters, my Dearest? down
+the street from
+where the public plaque reminds
+that of private
+loving the consequential chain
+trail is
+matters
+At night the states
+that it doesn't matter that I don't
+say them, remember
+them at the end of this claustro-
+phobic the
+dance, I wish I could see I wish
+I could
+dance her. At this night the states
+say them
+out there. That I am, am them
+indefinitely so and
+so wishful passive historic fated
+and matter-
+simple, matter-simple, an
+eyeful. I wish
+but I don't and little melody.
+Sorry that these
+little things don't happen any
+more. The states
+have drained their magicks
+for I have not
+seen them. Best not to tell. But
+you
+you would always remain, I
+trust, as I will
+always be alone.
+At night the states
+whistle. Anyone can live. I
+can. I am not doing any-
+thing doing this. I
+discover I love as I figure. Wed-
+nesday
+I wanted to say something in
+particular. I have been
+where. I have seen it. The God
+can. The people
+do some more.
+At night the states
+I let go of, have let, don't
+let
+Some, and some, in Florida, doing.
+What takes you so
+long? I am still with you in that
+part of the
+park, and vice will continue, but
+I'll have
+a cleaning Maine. Who loses
+these names
+loses. I can't bring it up yet,
+keeping my
+opinions to herself. Everybody in
+any room is a
+smuggler. I walked fiery and
+talked in the
+stars of the automatic weapons
+and partly for you
+Which you. You know.
+At night the states
+have told it already. Have
+told it. I
+know it. But more that they
+don't know, I
+know it too.
+At night the states
+whom I do stand before in
+judgment, I
+think that they will find
+me fair, not
+that they care in fact nor do
+I, right now
+though indeed I am they and
+we say
+that not that I've
+erred nor
+lost my way though perhaps
+they did (did
+they) and now he is dead
+but you
+you are not. Yet I am this
+one, lost
+again? lost & found by one-
+self
+Who are you to dare sing to me?
+At night the states
+accompany me while I sit here
+or drums
+there are alwavs drums what for
+so I
+won't lose my way the name of
+a
+personality, say, not California
+I am not
+sad for you though I could be
+I remember
+climbing up a hill under tall
+trees
+getting home. I guess we
+got home. I was
+going to say that the air was
+fair (I was
+always saying something like
+that) but
+that's not it now, and that
+that's not it
+isn't it either
+At night the states
+dare sing to me they who seem
+tawdry
+any more I've not thought I
+loved them, only
+you it's you whom I love
+the states are not good to me as
+I am to them
+though perhaps I am not
+when I think of your being
+so beautiful
+but is that your beauty
+or could it be
+theirs I'm having such a
+hard time remembering
+any of their names
+your being beautiful belongs
+to nothing
+I don't believe they should
+praise you
+but I seem to believe they
+should
+somehow let you go
+At night the states
+and when you go down to
+Washington
+witness how perfectly anything
+in particular
+sheets of thoughts what a waste
+of sheets at
+night. I remember something
+about an
+up-to-date theory of time. I
+have my
+own white rose for I have
+done
+something well but I'm not
+clear
+what it is. Weathered, perhaps
+but that's
+never done. What's done is
+perfection.
+At night the states
+ride the train to Baltimore
+we will try to acknowledge what was
+but that's not the real mirror
+is it? nor
+is it empty, or only my eyes
+are
+Ride the car home from Washington
+no
+they are not. Ride the subway
+home from
+Pennsylvania Station. The states
+are blind eyes
+stony smooth shut in moon-
+light. My
+French is the shape of this
+book
+that means I.
+At night the states
+the 14 pieces. I couldn't just
+walk on by. Why
+aren't they beautiful enough
+in a way that does not
+beg to wring
+something from a dry (wet)
+something
+Call my name
+At night the states
+making life, not explaining anything
+but all the popular songs say call
+my name
+oh call my name, and if I call
+it out myself to
+you, call mine out instead as our
+poets do
+will you still walk on by? I
+have
+loved you for so long. You
+died
+and on the wind they sang
+your name to me
+but you said nothing. Yet you
+said once before
+and there it is, there, but it is
+so still.
+Oh being alone I call out my
+name
+and once you did and do still in
+a way
+you do call out your name
+to these states whose way is to walk
+on by that's why I write too much
+At night the states
+whoever you love that's who you
+love
+the difference between chaos and
+star I believe and
+in that difference they believed
+in some
+funny way but that wasn't
+what I
+I believed that out of this
+fatigue would be
+born a light, what is fatigue
+there is a man whose face
+changes continually
+but I will never, something
+I will
+never with regard to it or
+never regard
+I will regard yours tomorrow
+I will wear purple will I
+and call my name
+At night the states
+you who are alive, you who are dead
+when I love you alone all night and
+that is what I do
+until I could never write from your
+being enough
+I don't want that trick of making
+it be coaxed from
+the words not tonight I want it
+coaxed from
+myself but being not that. But I'd
+feel more
+comfortable about it being words
+if it
+were if that's what it were for these
+are the
+States where what words are true
+are words
+Not myself. Montana. Illinois.
+Escondido.
+""",
+    """
+
+Poem 1 
+by Alice Notley
+
+
+St. Mark's Place caught at night in hot summer,
+Lonely from the beginning of time until now.
+Tompkins Square Park would be midnight green but only hot.
+I look through the screens from my 3rd floor apartment
+As if I could see something.
+Or as if the bricks and concrete were enough themselves
+To be seen and found beautiful.
+And who will know the desolation of St. Mark's Place
+With Alice Notley's name forgotten and
+This night never having been?
+""",
+    """
+
+Individual Time 
+by Alice Notley
+
+I'm calling out from pictures to your vision creating it
+turn right, that dream building cutglass window in door.
+Automatically inside their apartment, you don't have
+to get there. This is before the lost sacred corpus vision,
+someone says Look at my author photo. I
+don't really want to I'm turning to defiant metal
+not a dream part, can you see it where the movement of
+images turns back towards me I want a
+different, how I'm portrayed because you can't
+see me, visage. Look at me please. The soul is so thick
+larger than the portrait what you'd call madonnaesque,
+and then there was more hoax a view as I am
+the rose here. And you never wanted to be that, did I?
+I was waiting to see what I would be. Blackness
+eats you but your soul eats it without your knowing that
+figure, because it is causing your appearance to the world.
+They arrange me in clothes of Easter, or of
+the first day of classes, but I'm projecting pigment
+cracked gold on fire, thinking braver thoughts.
+It takes courage to get to the ancient altar
+of the moment where I create individual time.
+The picture body untremblingly stares large-eyed
+I also create the tablets of exponential seeing: it brightens
+all around it, as I'm the apparatus of what there is to be;
+and I am making it, my time visibly becoming me. 
+""",
+    """
+
+30th Birthday 
+by Alice Notley
+
+May I never be afraid
+especially of myself
+but
+Muhammed Ali are you telling
+the truth?
+Well you're being true aren't you and
+you talk so wonderfully in your body
+that protects you with physique of voice
+raps within dance
+May I never be afraid
+
+rocked and quaked
+the mantilla is lace
+whose black is oak
+But if I'm dark I'm strong
+as my own darkness
+my strength the universe
+whose blackness is air
+only starry
+lace
+But if I'm alive I'm strong
+as life
+Strong as the violets
+in Marlon Brando's fist
+his dissemblance flourished into truth
+She
+took them
+I'd take me too
+I do
+and my Ali I see you
+a hard bright speck of me
+the savage formalist
+authentic deed of gossip
+a kind body
+""",
+    """
+
+It Would 
+by Alice Notley
+
+
+it would be that
+but only if I knew how
+again
+Could something like
+that get lost? no only
+a little a little lost
+but if only I remember
+how I mean she or I
+oh a freight train goes by
+& they always do & did
+do
+I mean a real one too
+that I'm not on & am it
+very seriously
+in this serious love world
+that one
+where something oddly music
+will pass through your
+night
+and it will be me
+sweet me
+""",
+    """
+
+
+The Descent Of Alette ['The Water' 'Of The River'] 
+by Alice Notley
+
+'The water' 'of the river' 'was mild-temperatured,' 'the current
+gentle' 'I soon began' 'to swim—' 'in a moonless,' 'starless darkness'
+'The sky held no clouds—' 'no luminous' 'spheres existed here'
+'Yet the sky was' 'a sky; ' 'for the river air' 'was fresh & sweet'
+'Then,' 'as I swam,' 'the others I contained—' 'my companions
+from the subway—' 'weightlessly' 'emerged from me,' 'looking
+shadow-like,' '& quickly' 'solid-bodied' 'began to swim with me'
+'I never really' 'saw their faces' 'We swam quietly,' 'concentrating,'
+'our motions almost synchronized' 'In the distance sat' 'small yellow
+lights' 'where presumably' 'the other shore lay' 'Partway across'
+'the river,' 'something else, a substance,' 'a state of being,'
+'a thick noxious' 'distress' 'in the form of' 'a gray cloud,'
+'welled up within me' '& left my body' 'from a point along' 'my spine'
+'The cloud' 'hovered near us' '& so we turned onto' 'our backs to
+watch it' 'It was full of' 'ghoulish faces,' 'phosphorescent' 'death's
+heads; ' 'one skull grew large' '& open-mouthed' 'It had long' 'glowing
+hair,' 'screamed as if' 'in terror,' 'then spoke' 'to me:'
+' 'We are dying' 'You are killing us' 'killing us'' 'The cloud exploded'
+'into greenish flame' 'which was soon consumed' 'by darkness'
+'We turned over' '& resumed' 'our swimming'
+""",
+    """
+
+The Descent Of Alette ['Presently'] 
+by Alice Notley
+
+
+'Presently' 'we neared a pale beach,' 'narrow' 'with trees behind it'
+'thick & blue-black,' '& lights' 'many lights' 'deep inside the'
+'wooded land' 'I touched bottom' '& walked ashore' 'Then I saw'
+'a final entity,' 'airy image, pass' 'out of my body—' 'from my
+forehead' 'A small shape,' 'perhaps brain-shaped,' 'that seemed to'
+'contain in miniature' 'the cave network' 'where I had been'
+'I saw the rooms—' 'the caverns—' 'streaked with color,' 'dotted with
+lights,' 'but all tiny' 'as in a honeycomb' 'The image' 'receded,'
+'gently floated' 'away' 'on the wind,' 'like a flower' 'a hibiscus—'
+'all reds & darks' '& yellow glow—' 'Or like a lantern,' 'paper
+lantern' 'Then was gone.' 'The others' 'other swimmers' 'had not
+walked ashore' 'with me' 'I turned to find them' 'I thought they
+must be' 'still floating' 'in the water' 'But they had vanished,'
+'I was alone' 'Myself &' 'alone' 'Yet emptied' 'of much, it seemed'
+'I felt unburdened' '& even buoyant'
+""",
+    """
+
+
+The Goddess Who Created This Passing World 
+by Alice Notley
+
+Said Let there be lightbulbs & liquefaction
+Life spilled out onto the street, colors whirled
+Cars & the variously shod feet were born
+And the past & future & I born too
+Light as airmail paper away she flew
+To Annapurna or Mt. McKinley
+Or both but instantly
+Clarified, composed, forever was I
+Meant by her to recognize a painting
+As beautiful or a movie stunning
+And to adore the finitude of words
+And understand as surfaces my dreams
+Know the eye the organ of affection
+And depths to be inflections
+Of her voice & wrist & smile
+""",
+    """
+
+No World Is Intact 
+by Alice Notley
+
+
+and no one cares about you.
+I leaned down over
+don't care about, I care about
+you
+I leaned down over the
+world in portrayal
+of carefulness, answering
+something you couldn't say.
+walking or fallen and you
+were supposed
+to give therapy to me—
+me leaning down
+brushing with painted feathers
+to the left chance your operatic,
+broken
+book.
+""",
+    """
+
+
+Woman In Front Of Poster Of Herself 
+by Alice Notley
+
+
+Said I shouldn't.
+Fingering me.
+Everything I did.
+A litter of chewed knucklebones
+I've spread them out over the
+rectangular floor as regularly as
+I can; so I can account for them.
+Her hands are crossed over
+her breasts and each holds
+a feather; her face has no features
+Have I come to beg
+What do I wish — to be
+judged?
+Is it an accumulation
+of what I've said, that
+counts, that I'm counting
+is it all alphabet and abacus
+everything rhymed?
+You still don't have a face.
+Suddenly she has the face of a
+cat.
+No that's a different
+goddess.
+I tell you this
+bloodthirsty
+jaguar...
+I don't have any idea what my word is, I
+mean fault. Is it a word or an act. The
+whole thrill is ripping me apart
+Inside these
+words there's nothing but a pumping
+bloodsoaked...
+but clearly, everything I said, did,
+was a long shot
+We didn't hear a word
+What have you ever heard?
+Now I'm here — black-caped in a
+chair. Animal staring at me
+I sink into your disaffected
+ambiance to name.
+What emotional
+charges
+have been laid on me from
+earliest times
+and my own
+earliest
+resulting in the bone strewn carpet
+I had to grow the dice
+of accounting to your love; for
+you made me speak to you
+lovingly; or did I do that naturally
+oh just, bloodthirsty face
+who doesn't have to understand.
+I don't know who I'm speaking to
+is pushing me
+Judgment maybe it's when being
+fragile I
+hallucinate you best
+I don't want to use my name!
+'Where I was born we girls ran
+free. and named ourselves,'
+Justice says.
+She may kill me,
+it depends on whether she's hungry
+""",
+    """
+
+Poem 2 
+by Alice Notley
+
+
+You hear that heroic big land music?
+Land a one could call one.
+He starred, had lives, looks down:
+windmill still now they buy only
+snow cows. Part of a dream, she
+had a long waist he once but yet
+never encircled, and now I'm
+in charge of this, this donkey with
+a charmed voice. Elly, I'm
+being sad thinking of Daddy.
+He marshaled his private lady,
+did she wear a hat or the
+other side? get off my own land? We
+were all born on it to die on
+with no writin' on it. But who are
+you to look back, well he's
+humming 'From this valley,' who's gone.
+Support and preserve me, father. Oh
+Daddy, who can stand it?
+""",
+    """
+
+My Sea 
+by Alice Notley
+
+
+What I lose you let me, accusation
+always gets one in. But I want to talk like the dead
+remember that town where we went or
+how do I know when I'm just a soul — not
+when I'm leading?
+A soul can lead, fight and kill; in the sketchy rain
+there, but you can't kill where we're dead. That's
+the best thing — no one has any power.
+How can I lead you without power? We want to find
+out ... Drop everything? — there's no gravity.
+Are you grave? There's no bravery.
+I'm going to lead you
+into a you you don't know ... Most people want
+to go. There we risked being wrong but
+that was a linguistic quality, or you
+could have brought us to hell. Outside
+of gravity, instead, is the house. It wasn't
+built with raisins. The light there, but
+what's it for? For eyes. He called me "Four Eyes"
+now I have billions. It's a house on the coast
+Is it the House of Answers? I will continue
+to reason for you, living on no particular income
+deep in my soul. The house has a basement I
+didn't know about
+connecting to My Sea, Mare Meum. The answers
+break with foam and wild pearls. The wind
+is me too — you know who you are — where's
+the desert? The sea came back to this land,
+to the abandoned its lover. The kind of sea
+you can't drown in. You can want to coincide with me
+for I am the soul your leader the clear rock of kind mind,
+senseless. Senseless free will — the only thing here.
+""",
+    """
+
+Perhaps Not For You
+by Alice Notley
+
+There is
+no
+audience
+because
+there is
+no audience.
+
+So if you speak only to
+imagined beings
+what does 'only' mean?
+
+- - - - - - - - - - - - -
+
+This building formerly a restaurant . . .
+this small room has been scraped of its paint
+and denuded of most former furniture: but
+also it has grown in size—can a building be
+enticed to grow? Because it is now as big as an
+airplane hangar.
+
+- - - - - - - - - - - - -
+
+Your
+beautiful face
+unbloodied beneath
+flies
+
+Mother of flies your
+beauty
+to turn to. If only
+the audience
+could see how
+you are peaceful and the
+flies
+languid, glossy
+
+But the audience will still bring
+its own feelings
+to these
+words
+
+not seeing you
+not seeing
+what I
+am present for.
+
+- - - - - - - - - - - - -
+
+Who has left me
+here, I have.
+
+Who are your
+familiars
+
+Come
+into the
+enlarging
+page if you dare
+
+- - - - - - - - - - - - -
+
+Because he invented
+your shape I do mean
+structure
+
+because he invented you badly
+
+everything is still hidden.
+
+- - - - - - - - - - - - -
+
+I was to impale myself on a
+quadrangular
+steel rod, with a blunt end
+with a blunt end
+which would make puncture
+more difficult
+and I tried—it's too hard. I can't
+Okay said the voice. I can't
+Okay
+
+then I was weeping
+But it's blood! I'm
+crying blood! I
+screamed
+
+That's part of it
+said the voice.
+
+- - - - - - - - - - - - - -
+
+I think this is hard.
+(That's part of it)
+
+How they prefer him must go.
+
+I think this is difficult singing
+
+Length and repetition
+create power
+
+If this voice can return like
+a body
+
+It resembles something that's already been,
+
+Changing.
+
+- - - - - - - - - - - - - - -
+
+Chestnuts broken
+autumnal fungi
+so you will remember, that
+it's fall
+outside
+falling. you'll go down
+
+this is no story for the puling
+social classes
+No not at all
+it's for us my familiars say
+who let me weep blood on their ground. """,
+]
